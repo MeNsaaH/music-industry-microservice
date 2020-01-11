710c703709c6a37556a7e64633071b1398291dad
Manasseh Mmadu <mmadumanasseh@gmail.com>
2020-01-11 19:37:37 +0100

    :sparkles: Added email services (both grpc Client and Server) and Proto files

---
+++ email/email_client.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import grpc
+import sys
+
+import app_pb2
+import app_pb2_grpc
+
+import logging
+from pythonjsonlogger import jsonlogger
+
+logger = logging.getLogger('emailservice-client')
+
+formatter = jsonlogger.JsonFormatter()
+logHandler = logging.StreamHandler(sys.stdout)
+logHandler.setFormatter(formatter)
+logger.addHandler(logHandler)
+
+
+def send_confirmation_email(email, message):
+  channel = grpc.insecure_channel('0.0.0.0:8081')
+  stub = app_pb2_grpc.EmailServiceStub(channel)
+  try:
+    response = stub.SendConfirmation(app_pb2.SendConfirmationRequest(
+      email = email,
+      message = message 
+    ))
+    logger.info('Request sent.')
+  except grpc.RpcError as err:
+    logger.error(err.details())
+    logger.error('{}, {}'.format(err.code().name, err.code().value))
+
+if __name__ == '__main__':
+  logger.info('Client for email service.')
+  send_confirmation_email("mmadumanasseh@gmail.com", "This is a confirmation")
---
+++ email/email_server.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+
+from concurrent import futures
+import argparse
+import os
+import sys
+import time
+import grpc
+from jinja2 import Environment, FileSystemLoader, select_autoescape, TemplateError
+
+import app_pb2
+import app_pb2_grpc
+from grpc_health.v1 import health_pb2
+from grpc_health.v1 import health_pb2_grpc
+
+import logging
+from pythonjsonlogger import jsonlogger
+
+logger = logging.getLogger('emailservice-server')
+
+formatter = jsonlogger.JsonFormatter()
+logHandler = logging.StreamHandler(sys.stdout)
+logHandler.setFormatter(formatter)
+logger.addHandler(logHandler)
+
+# Loads confirmation email template from file
+env = Environment(
+    loader=FileSystemLoader(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+template = env.get_template('confirmation.html')
+
+class BaseEmailService(app_pb2_grpc.EmailServiceServicer):
+  def Check(self, request, context):
+    return health_pb2.HealthCheckResponse(
+      status=health_pb2.HealthCheckResponse.SERVING)
+
+  def Watch(self, request, context):
+    yield health_pb2.HealthCheckResponse(
+      status=health_pb2.HealthCheckResponse.SERVING)
+
+class EmailService(BaseEmailService):
+  def __init__(self):
+    raise Exception('cloud mail client not implemented')
+    super().__init__()
+
+  @staticmethod
+  def send_email(client, email_address, content):
+    response = client.send_message(
+      sender = client.sender_path(project_id, region, sender_id),
+      envelope_from_authority = '',
+      header_from_authority = '',
+      envelope_from_address = from_address,
+      simple_message = {
+        "from": {
+          "address_spec": from_address,
+        },
+        "to": [{
+          "address_spec": email_address
+        }],
+        "subject": "Your Confirmation Email",
+        "html_body": content
+      }
+    )
+    logger.info("Message sent: {}".format(response.rfc822_message_id))
+
+  def SendConfirmation(self, request, context):
+    email = request.email
+    order = request.order
+
+    try:
+      confirmation = template.render(order = order)
+    except TemplateError as err:
+      context.set_details("An error occurred when preparing the confirmation mail.")
+      logger.error(err.message)
+      context.set_code(grpc.StatusCode.INTERNAL)
+      return app_pb2.Empty()
+
+    try:
+      EmailService.send_email(self.client, email, confirmation)
+    except GoogleAPICallError as err:
+      context.set_details("An error occurred when sending the email.")
+      print(err.message)
+      context.set_code(grpc.StatusCode.INTERNAL)
+      return app_pb2.Empty()
+
+    return app_pb2.Empty()
+
+class DummyEmailService(BaseEmailService):
+  def SendConfirmation(self, request, context):
+    logger.info('A request to send order confirmation email to {} has been received.'.format(request.email))
+    return app_pb2.Empty()
+
+class HealthCheck():
+  def Check(self, request, context):
+    return health_pb2.HealthCheckResponse(
+      status=health_pb2.HealthCheckResponse.SERVING)
+
+def start(dummy_mode):
+  server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+  service = None
+  if dummy_mode:
+    service = DummyEmailService()
+  else:
+    raise Exception('non-dummy mode not implemented yet')
+
+  app_pb2_grpc.add_EmailServiceServicer_to_server(service, server)
+  health_pb2_grpc.add_HealthServicer_to_server(service, server)
+
+  port = os.environ.get('PORT', "8081")
+  logger.info("listening on port: "+port)
+  server.add_insecure_port('[::]:'+port)
+  server.start()
+  try:
+    while True:
+      time.sleep(3600)
+  except KeyboardInterrupt:
+    server.stop(0)
+
+
+if __name__ == '__main__':
+  logger.info('starting the email service in dummy mode.')
+  start(dummy_mode = True)
